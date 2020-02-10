1803b38f249a8a59fb22a0757629c64f8c60c6a2
pratap54321 <rampratap.jaiswal@mirraw.com>
2020-02-10 20:20:59 +0530

    Updates

---
+++ precache-manifest.de59bd042b887509e4c5c2f0a4de3c7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdd574e77362a77ccd296c3d90eeccf",
+    "url": "/pratap54321/react-deploye.git/index.html"
+  },
+  {
+    "revision": "96ff9b6c837414b0f72a",
+    "url": "/pratap54321/react-deploye.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f177694d75d5c79b4b4",
+    "url": "/pratap54321/react-deploye.git/static/js/2.a1c7a29e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pratap54321/react-deploye.git/static/js/2.a1c7a29e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ff9b6c837414b0f72a",
+    "url": "/pratap54321/react-deploye.git/static/js/main.b7a7480c.chunk.js"
+  },
+  {
+    "revision": "29229c30401026748102",
+    "url": "/pratap54321/react-deploye.git/static/js/runtime-main.0781363b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pratap54321/react-deploye.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
